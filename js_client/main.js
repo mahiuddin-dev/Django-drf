826e5bf0c94d3db18a268d31036d33ce36ea9375
codermahiuddin <codermahiuddin@gmmail.com>
2022-03-25 17:03:33 +0600

    Js lient token and access

--- js_client/main.js
+++ js_client/main.js
@@ -1,4 +1,5 @@
 const loginForm = document.querySelector('#loginForm');
+const content = document.querySelector('#content');
 const baseEndpoint = "http://localhost:8000/api"
 
 if (loginForm) {
@@ -25,10 +26,62 @@ function handleLogin(event) {
         return response.json()
     })
     .then(authData => {
-        // handleAuthData(authData, getProductList)
-        console.log(authData);
+        handleAuthData(authData, getProductList)
     })
     .catch(err=> {
         console.log('err', err)
     })
 }
+
+function handleAuthData(authData,callback){
+    localStorage.setItem('access', authData.access)
+    localStorage.setItem('refresh', authData.refresh)
+
+    if(callback){
+        callback();
+    }
+}
+
+function writeContent(data){
+    if(content){
+        content.innerHTML = "<pre>"+JSON.stringify(data,null,4)+"</pre>"
+    }
+}
+
+function getFetchOptions(method, body){
+    return {
+        method: method === null ? "GET" : method,
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem('access')}`
+        },
+        body: body ? body : null
+    }
+}
+
+function isTokenNotValid(jsonData) {
+    if (jsonData.code && jsonData.code === "token_not_valid"){
+        alert("Please login again")
+        return false
+    }
+    return true
+}
+
+
+function getProductList(){
+    const endpoint = `${baseEndpoint}/product/`
+
+    const options = getFetchOptions()
+    fetch(endpoint, options)
+    .then(response=> response.json())
+    .then(data=> {
+        const validData = isTokenNotValid(data)
+        if (validData) {
+            writeToContainer(data)
+        }
+    })
+    .catch(err=> {
+        console.log('err', err)
+    })
+}
+getProductList()
